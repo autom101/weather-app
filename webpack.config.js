bc00323916c045723e9e66267aa084d682f817da
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-02 15:24:44 -0600

    Add initial files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  watch: false,
+  watchOptions: {
+    ignored: "**/node_modules",
+  },
+  entry: {
+    index: "./src/js/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather App",
+    }),
+  ],
+  output: {
+    filename: "[name].main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
