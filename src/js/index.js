f2dcf3954b2cfdb90cc6109ed02dc95c055a9c18
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-02 15:50:11 -0600

    Get weather data from hard-coded city and print to console

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,22 @@
 //Import css
-import "reset.css";
-import "styles.css";
+import "../css/reset.css";
+import "../css/styles.css";
 
-console.log("hi");
+//Import js
+import config from "../../config.js";
+
+const getWeather = async () => {
+  const fetchWeather = await fetch(
+    "http://api.weatherapi.com/v1/current.json?key=" +
+      config.weatherApi_key +
+      "&q=" +
+      "London",
+    {
+      mode: "cors",
+    }
+  );
+  const weatherObj = await fetchWeather.json();
+  console.log(weatherObj);
+};
+
+getWeather();
